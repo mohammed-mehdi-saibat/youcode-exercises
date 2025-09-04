084ca0c642726c047ae354f7c0b85dbab4e7b7d8
saibat01 <mohammedmehdisaibat@gmail.com>
2025-09-04 23:14:01 +0100

    Challenge 1: Initialisation et affichage - resolue

---
+++ tableaux/challenge1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int  main() {
+  int tableau[] = {1,2,3,4,5};
+  int taille = sizeof(tableau) / sizeof(tableau[0]);
+
+  for(int i = 0; i < taille; i++) {
+    printf("%d\n", tableau[i]);
+    // printf("\n");
+  }
+}
